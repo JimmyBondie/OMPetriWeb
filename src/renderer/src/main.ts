d43c8bb71b644a052202f8f32ff486dee215e569
Leon Maximilian Kassebaum <leon.kassebaum@uni-bielefeld.de>
2024-04-20 02:09:46 +0200

    Added a basic menu bar and translated the items

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -9,6 +9,10 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+const vuetify = createVuetify({
+  components,
+  directives
+})
 
 // Material Design Icons
 import '@mdi/font/css/materialdesignicons.css'
@@ -16,12 +20,23 @@ import '@mdi/font/css/materialdesignicons.css'
 // Vue Store
 import store from './store'
 
-const vuetify = createVuetify({
-  components,
-  directives
+// Vue-I18n
+import { createI18n } from 'vue-i18n'
+import en from './locales/en.json'
+import de from './locales/de.json'
+const i18n = createI18n({
+  silentFallbackWarn: true,
+  silentTranslationWarn: true,
+  locale: navigator.language,
+  fallbackLocale: 'en',
+  messages: {
+    en: en,
+    de: de
+  }
 })
 
 const app = createApp(App)
+app.use(i18n)
 app.use(router)
 app.use(vuetify)
 app.use(store)
--- src/renderer/src/views/HomeView.vue
+++ src/renderer/src/views/HomeView.vue
@@ -2,16 +2,50 @@
 
 <template>
   <main>
-    <v-toolbar density="comfortable" color="blue">
+    <v-app-bar density="comfortable" color="blue">
       <template v-slot:prepend>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon variant="text" @click.stop="menuOpen = !menuOpen"></v-app-bar-nav-icon>
       </template>
 
       <v-app-bar-title>OMPetriWeb</v-app-bar-title>
-    </v-toolbar>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="menuOpen"
+      :location="$vuetify.display.mobile ? 'bottom' : undefined"
+    >
+      <v-list v-model:selected="tabs" nav>
+        <v-list-item prepend-icon="mdi-graph-outline" value="overview">
+          {{ $t('Overview') }}
+        </v-list-item>
+        <v-list-item prepend-icon="mdi-cog-outline" value="settings">
+          {{ $t('Settings') }}
+        </v-list-item>
+        <v-list-item prepend-icon="mdi-information-outline" value="about">
+          {{ $t('About') }}
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-window v-model="tabs" direction="vertical">
+        <v-window-item value="overview">Graph</v-window-item>
+        <v-window-item value="settings">Settings</v-window-item>
+        <v-window-item value="about">About</v-window-item>
+      </v-window>
+    </v-main>
   </main>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      menuOpen: false,
+      tabs: []
+    }
+  }
+}
+</script>
 
 <style lang="scss"></style>
